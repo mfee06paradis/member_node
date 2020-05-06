e0fd28a79b2c4f933bd998427e651f2deae2cd9a
mfee06paradis <64397614+mfee06paradis@users.noreply.github.com>
2020-05-06 20:02:34 +0800

    member node

---
+++ routes/members.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const moment = require('moment-timezone');
+const db = require(__dirname + '/../db_connect');
+const router = express.Router();
+
+
+router.get('/', (req, res)=>{
+    
+  const sql = "SELECT * FROM members";
+  db.query(sql, (error, results, fields)=>{
+      if(error){
+          console.log(error);
+      } else {
+          res.json(results);
+      }
+  });
+
+});
+
+router.post('/add', (req, res)=>{
+    const output = {
+        success: false,
+        error: '',
+    };
+    // TODO: 應該檢查表單進來的資料
+
+    const sql = "INSERT INTO `members`(`MemberName`, `MemberPW`, `MemberEmail`) VALUES (?,?,?)";
+
+    db.queryAsync(sql, [
+        req.body.memberName,
+        req.body.memberPW,
+        req.body.memberEmail,
+    ])
+        .then(results=>{
+            output.results = results;
+            if(results.affectedRows===1){
+                output.success = true;
+            }
+            res.json(output);
+        })
+        .catch(ex=>{
+            console.log('ex:', ex);
+        })
+
+    //res.json(req.body);
+});
+
+
+
+
+
+
+module.exports = router;
