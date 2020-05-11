efc8ddff0043085d1c941ff6e53b083e63e4aac0
mfee06paradis <64397614+mfee06paradis@users.noreply.github.com>
2020-05-11 15:52:11 +0800

    Add files via upload

--- routes/members.js
+++ routes/members.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const moment = require('moment-timezone');
 const db = require(__dirname + '/../db_connect');
@@ -5,7 +6,6 @@ const router = express.Router();
 
 
 router.get('/', (req, res)=>{
-    
   const sql = "SELECT * FROM member";
   db.query(sql, (error, results, fields)=>{
       if(error){
@@ -14,7 +14,6 @@ router.get('/', (req, res)=>{
           res.json(results);
       }
   });
-
 });
 
 router.post('/add', (req, res)=>{
@@ -22,7 +21,6 @@ router.post('/add', (req, res)=>{
         success: false,
         error: '',
     };
-    // TODO: 應該檢查表單進來的資料
 
     const sql = "INSERT INTO `member`(`memberName`, `memberPw`, `memberEmail`) VALUES (?,?,?)";
 
@@ -41,13 +39,36 @@ router.post('/add', (req, res)=>{
         .catch(ex=>{
             console.log('ex:', ex);
         })
-
-    //res.json(req.body);
 });
 
+router.put('/update', (req, res)=>{
+    const output = {
+        success: false,
+        error: '',
+    };
 
+    const sql = "UPDATE `member` SET `memberName` = ?, `memberSex` = ?, `memberBirthday`=?, `memberPhone` = ?, `memberAddress`=? WHERE `memberId` = ?";
 
+    db.queryAsync(sql, [
+        req.body.name,
+        req.body.gender,
+        req.body.birthday,
+        req.body.phone,
+        req.body.addressData,
+        req.body.memberID
+    ])
+        .then(results=>{
+            output.results = results;
+            if(results.affectedRows===1){
+                output.success = true;
+            }
+            res.json(output);
+        })
+        .catch(ex=>{
+            console.log('ex:', ex);
+        })
 
-
+});
 
 module.exports = router;
+
